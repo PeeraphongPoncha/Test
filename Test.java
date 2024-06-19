3ef7e2917f900c3ac14c59cbe8b8280393c2e10c
PeeraphongPoncha <peeraphong.p@kkumail.com>
2024-06-19 16:29:25 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,4 +2,16 @@ public class Test{
    public test{}
    
 
+    public static void main(String[] args) {
+         float x = 1;
+         int y = 2;
+         float newx ;
+         for(int i = 1; i<6 ;i++){
+             newx = (x+(y/x))/2;
+             System.out.print(i);
+             System.out.print("  ");
+             System.out.println(newx);
+             x = newx;
+             }
+    }
 }
